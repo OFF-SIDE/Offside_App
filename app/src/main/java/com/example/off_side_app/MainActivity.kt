857b1226009786b21b35ccccf79f9de7003ae775
hk47188@naver.com <wawa4044@@>
2023-11-01 22:10:06 +0900

    day1_3

--- app/src/main/java/com/example/off_side_app/MainActivity.kt
+++ app/src/main/java/com/example/off_side_app/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.off_side_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.off_side_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.imageButton2.setOnClickListener{
+            val intent = Intent(this, MainPageActivity1::class.java)
+            startActivity(intent)
+        }
     }
+
 }
---
+++ app/src/main/java/com/example/off_side_app/MainPageActivity1.kt
@@ -0,0 +1,11 @@
+package com.example.off_side_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainPageActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_page1)
+    }
+}
