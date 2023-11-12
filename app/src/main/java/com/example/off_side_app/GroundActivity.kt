85186338a862a9abe5acbb5a66680a2b25e8913a
hk47188@naver.com <wawa4044@@>
2023-11-12 16:55:25 +0900

    day4_1

--- app/src/main/java/com/example/off_side_app/GroundActivity.kt
+++ app/src/main/java/com/example/off_side_app/GroundActivity.kt
@@ -1,86 +1,48 @@
 package com.example.off_side_app
 
-import android.content.ContentValues
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.ImageDecoder
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.off_side_app.databinding.ActivityGroundBinding
-import java.text.SimpleDateFormat
 
 class GroundActivity : AppCompatActivity() {
 
-    val PERM_STORAGE = 9
-
-    val REQ_GALLERY = 10
-
-    val binding by lazy{ ActivityGroundBinding.inflate(layoutInflater) }
+    lateinit var binding: ActivityGroundBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityGroundBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-    }
+        binding.selectImageBtn.setOnClickListener {
 
-    fun setViews(){
-        binding.selectImageBtn.setOnClickListener{
-            openGallery()
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            activityResult.launch(intent)
+
         }
     }
 
-    var realUri: Uri? = null
+        private val activityResult: ActivityResultLauncher<Intent> = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()){
 
-    fun openGallery(){
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = MediaStore.Images.Media.CONTENT_TYPE
-        startActivityForResult(intent, REQ_GALLERY)
-    }
+                if(it.resultCode == RESULT_OK && it.data != null){
 
-    fun createImageUri(filename:String, mimeType:String) : Uri?{
-        val values = ContentValues()
-        values.put(MediaStore.Images.Media.DISPLAY_NAME, filename)
-        values.put(MediaStore.Images.Media.MIME_TYPE, mimeType)
+                    val uri = it.data!!.data
 
-        return contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values)
+                    Glide.with(this)
+                        .load(uri)
+                        .into(binding.pictureImageView)
 
-    }
-
-    fun newfileName() : String{
-        val sdf = SimpleDateFormat("yyyyMMdd_HHmmss")
-        val filename = sdf.format(System.currentTimeMillis())
-        return "${filename}.jpg"
-    }
-
-    fun loadBitmap(photoUri: Uri): Bitmap? {
-        try{
-            return if(Build.VERSION.SDK_INT > Build.VERSION_CODES.O_MR1){
-                val source = ImageDecoder.createSource(contentResolver, photoUri)
-                ImageDecoder.decodeBitmap(source)
-            }else{
-                MediaStore.Images.Media.getBitmap(contentResolver, photoUri)
-            }
-        } catch(e:Exception){
-            e.printStackTrace()
-        }
-        return null
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == RESULT_OK){
-           when(requestCode){
-                REQ_GALLERY -> {
-                    data?.data?.let { uri ->
-                        binding.pictureImageView.setImageURI(uri)
                 }
-            }    
         }
-        }
-    }
 }
+
+
+
+
 
 
---
+++ app/src/main/java/com/example/off_side_app/SubGroundActivity.kt
@@ -0,0 +1,12 @@
+package com.example.off_side_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SubGroundActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub_ground)
+
+    }
+}
